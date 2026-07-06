6c287b446eae51bb49c8d7be879f71bd868a80a0
gogoulet <jfgoulet.consultation@gmail.com>
2026-07-06 14:14:17 -0400

    Remove comments from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Ton domaine GitHub Pages principal
   site: 'https://gogoulet.github.io',
-  // Le nom exact de ton dépôt GitHub (très important !)
   base: '/dev_ares',
 });
