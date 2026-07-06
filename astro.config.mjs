d17e79a7ca97eb711edfba28219cf7b2abe66c8d
gogoulet <jfgoulet.consultation@gmail.com>
2026-07-06 14:34:02 -0400

    Update base path in astro.config.mjs
    Removed the trailing slash from the base path in the Astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://gogoulet.github.io',
-  base: '/dev_ares/',
+  base: '/dev_ares', // Pas de point, juste le nom du repo encadré par des slashes
 });
